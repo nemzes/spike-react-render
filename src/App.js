a442742a81bf0cada48846e15f8ec2d8864fbb19
Nelson Menezes <nemzes@gmail.com>
2021-03-04 21:28:55 +0100

    Implement test case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef, useState } from "react";
+import "./App.css";
+
+const rows = [...Array(1000).keys()];
+const cols = [...Array(10).keys()];
+
+function MyComponent({ callback, children }) {
+  const ref = useRef();
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+    callback(ref.current);
+  }
+  return (
+    <div ref={ref} onClick={handleClick} className="blah">
+      {children} ({clicks})
+    </div>
+  );
+}
 
 function App() {
+  const anchor = useRef(null);
+  const handleClick = useCallback(
+    (element) => {
+      if (!anchor) {
+        console.log('setting anchor for first time')
+      }
+      console.log(element);
+      anchor.current = element;
+    },
+    []
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row}>
+              {cols.map((col) => (
+                <td key={col}>
+                  <MyComponent callback={handleClick}>
+                    {row}-{col}
+                  </MyComponent>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
